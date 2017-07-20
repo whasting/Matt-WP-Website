70527078f5a3a8d0bc3ebb3f4fa05ee7eb9ad4b9
whasting <hastingswade1@gmail.com>
2017-07-19 22:52:13 -0700

    work on gallery

--- wp-content/themes/photoblog/gallery.php
+++ wp-content/themes/photoblog/gallery.php
@@ -2,24 +2,56 @@
   /**
    * Template Name: Gallery
    */
-  get_header();
+  // get_header();
 ?>
 
 <div id="main-content" class="main-content">
 
-<?php
-	
-?>
+  <div id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+      <h1>Hey There</h1>
 
-			<h1>Hello There</h1>
+      <?php
+        // TO SHOW THE PAGE CONTENTS
+        while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+            <div class="entry-content-page">
+                <?php the_content(); ?> <!-- Page Content -->
+            </div><!-- .entry-content-page -->
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+      <?php
+        endwhile; //resetting the page loop
+        wp_reset_query(); //resetting the page query
+      ?>
+
+      <?php
+   // TO SHOW THE POST CONTENTS
+   ?>
+       <?php
+       $my_query = new WP_Query( 'cat=1' ); // I used a category id 1 as an example
+       ?>
+       <?php if ( $my_query->have_posts() ) : ?>
+       <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+       <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+           <h1 class="entry-title"><?php the_title(); ?></h1> <!-- Queried Post Title -->
+           <div class="entry-content">
+               <?php the_excerpt(); ?> <!-- Queried Post Excerpts -->
+           </div><!-- .entry-content -->
+
+       <?php endwhile; //resetting the post loop ?>
+
+       </div><!-- #post-<?php the_ID(); ?> -->
+
+       <?php
+       wp_reset_postdata(); //resetting the post query
+       endif;
+       ?>
+    </div><!-- #content -->
+  </div><!-- #primary -->
 </div><!-- #main-content -->
 
+
 <?php
-  get_footer();
+  // get_footer();
 ?>
